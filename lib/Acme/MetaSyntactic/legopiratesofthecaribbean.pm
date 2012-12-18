5a5285316d67677c4f723ef3c56d36d6ed11bca5
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2012-12-18 10:43:36 +0000

    Me hearties yo ho

---
+++ lib/Acme/MetaSyntactic/legopiratesofthecaribbean.pm
@@ -0,0 +1,101 @@
+package Acme::MetaSyntactic::legopiratesofthecaribbean;
+
+#ABSTRACT: LEGO Pirates of the Caribbean theme
+
+use strict;
+use warnings;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+
+q[You-Know-Who];
+
+=pod
+
+=head1 DESCRIPTION
+
+Characters from the LEGO Pirates of the Caribbean video game as gleaned from
+the L<http://www.gamefaqs.com/> walkthroughs.
+
+I<Me hearties yo ho!>
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+Jack_Sparrow
+Will_Turner
+Mr_Brown
+Guard_Dog
+Mr_Gibbs
+Anamaria
+Marty
+Cotton
+Elizabeth_Swann
+Elizabeth
+Pintel
+Ragetti
+Bootstrap_Bill_Turner
+James_Norrington
+Norrington
+Elizbaeth
+Captain_Elizabeth
+Captain_Barbossa
+Sao_Feng
+Tia_Dalma
+Murtogg
+Mullroy
+Tai_Huang
+Tattoo_Pirate
+Scrum
+Cook
+Salaman
+Garheng
+Angelica
+Privateer_Barbossa
+Syrena
+Lieutenant_Groves
+Koehler
+Twigg
+Mallot
+Scratch
+Bosun
+Clubba
+Grapple
+Jacoby
+Hungry_Cannibal
+Angry_Cannibal
+Governor_Weatherby_Swann
+Captain_Bellamy
+Maccus
+Clanker
+Jimmy_Legs
+Koleniko
+Wyvern
+Angler
+Hadras
+Crash
+Penrod
+Lian
+Park
+Mr_Mercer
+Mistress_Ching
+Sri_Sumbhajee_Angria
+Gentleman_Jocard
+Ammand_the_Corsair
+Eduardo_Villanueva
+Captaine_Chevalle
+Lord_Cutler_Beckett
+Davy_Jones
+Ancient_Sailor
+King_George
+Quartermaster
+Gunner
+Derrick
+Philip
+The_Spaniard
+Blackbeard
